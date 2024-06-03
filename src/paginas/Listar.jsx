f8538686810d9b21ac5d8aa7f05fbe971b7d3829
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-06-02 20:29:51 -0500

    Modulo veterinario, seccion crear y listar (CRUD) completada, continuar desde la seccion 2 del modulo veterinario

--- src/paginas/Listar.jsx
+++ src/paginas/Listar.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Tabla from '../componets/Tabla'
+// se listan solo los pacintes de estado activo
+import React from 'react';
+import Tabla from '../componets/Tabla';
 
 const Listar = () => {
-    return (
-        <div>
-            <h1 className='font-black text-4xl text-gray-500'>Listar...</h1>
-            <hr className='my-4' />
-            <p className='mb-8'>Este módulo te permite registrar listar .....</p>
-            <Tabla/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="font-black text-4xl text-gray-500">Pacientes</h1>
+      <hr className="my-4" />
+      <p className="mb-8">Estos son los pacientes con estado activo</p>
+      <Tabla />
+    </div>
+  );
+};
 
-export default Listar
+export default Listar;
