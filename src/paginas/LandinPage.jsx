d11ad014a6d464bd4f51525ec9e32005b9850334
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-07-07 19:02:32 -0500

    Se eliminan los recursos no utilizados

--- src/paginas/LandinPage.jsx
+++ src/paginas/LandinPage.jsx
@@ -1,17 +1,7 @@
 import logoDarkMode from '../assets/dark.png';
-import logoFacebook from '../assets/ig.jpg';
 import logoGithub from '../assets/github.png';
 import logoLinkedind from '../assets/linkedin.png';
 import logoRocket from '../assets/rocket.webp';
-import logoCode from '../assets/code.png';
-import logoConsulting from '../assets/consulting.png';
-import logoDesign from '../assets/design.png';
-import logoWeb1 from '../assets/web1.png';
-import logoWeb2 from '../assets/web2.png';
-import logoWeb3 from '../assets/web3.png';
-import logoWeb4 from '../assets/web4.png';
-import logoWeb5 from '../assets/web5.png';
-import logoWeb6 from '../assets/web6.png';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
